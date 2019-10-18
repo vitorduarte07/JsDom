e4d4b5c7ef791f977ebb33b27b6d0d838da40b7f
vitorduarte07 <55919897+vitorduarte07@users.noreply.github.com>
2019-10-17 21:47:11 -0300

    atualizando meu projeto

--- assets/js/exercicio01.js
+++ assets/js/exercicio01.js
@@ -1,13 +1,51 @@
 
-function simuladorSalario(){
+function simuladorSalario() {
     const SALARIO_COMERCIAL = 1300;
     const PERCENTUAL_COMISSAO = 0.1;
-    
+    const META_VENDA = 1000;
+
     var nomeVendedor = document.getElementById('nomeVendedor').value;
     var totalVendas = document.getElementById('totalVendas').value;
-    
-    var salarioBruto = SALARIO_COMERCIAL + (totalVendas * PERCENTUAL_COMISSAO);
-    console.log('Nome Vendedor: ' + nomeVendedor);
-    console.log('Total de Vendas: ' + totalVendas);
-    console.log('Salário Bruto: ' + salarioBruto);
+ 
+
+    if (totalVendas >= META_VENDA) {
+        var salarioBruto = SALARIO_COMERCIAL + (totalVendas * PERCENTUAL_COMISSAO);
+        var comissao = (totalVendas * PERCENTUAL_COMISSAO);
+        exibirMensagemSucesso(nomeVendedor, salarioBruto, comissao);
+        }else{
+            exibirMensagemErro();
+        }
+/*
+    <div class="col-sm-12 col-md-9 col-lg-9 pt-30">
+        <div class="alert alert-success" role="alert">
+            <h3> A simple success alert—check it out! </h3>
+        </div>
+    </div>
+    <div class="alert alert-danger" role="alert">
+    A simple danger alert—check it out!
+    </div>
+    */
+
+    function exibirMensagemSucesso(nome, salario, comissao){
+        let html = '<div class="col-sm-12 col-md-9 col-lg-9 pt-30">' +
+                    '<div class="alert alert-success" role="alert">' +
+                    '<h4>'+ nome +', seu salário bruto será de '+ salario +' reais.<br> Você recebeu '+ comissao +' reais de comissão</h4>' +
+                    '</div>' +
+                '</div>';  
+                let mensagemResultado = document.getElementById('mensagemResultado');
+                mensagemResultado.className = 'row visible';
+                mensagemResultado.innerHTML = html;
+
+    }
+
+    function exibirMensagemErro(){
+        let html = '<div class="col-sm-12 col-md-9 col-lg-9 pt-30">' +
+                    '<div class="alert alert-danger" role="alert">' +
+                    '<h4> Você não alcançou a meta de vendas desejada</h4>' +
+                    '</div>' +
+                    '</div>';
+                    let mensagemResultado = document.getElementById('mensagemResultado');
+                    mensagemResultado.className = 'row visible';
+                    mensagemResultado.innerHTML = html;
+    }
 }
